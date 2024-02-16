77967bef4902e25defb83619e8c76415e745e3ff
Moses Hunsu <hunsumoses7@gmail.com>
2024-02-16 11:08:11 +0100

    Worked on PropTypes as an alternative to TypeScript

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerstyle = {
   display: "flex",
@@ -8,6 +9,16 @@ const containerstyle = {
 
 const starContainerStyle = {
   display: "flex",
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
@@ -24,7 +35,7 @@ export default function StarRating({
 
   function handleRating(rating) {
     setRating(rating);
-    onSetRating(rating);
+    onSetRating && onSetRating(rating);
   }
 
   const textStyle = {
