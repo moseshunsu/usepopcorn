8ab466c74bd8fb319e85eb8018d4ff62c4ad8bd1
Moses Hunsu <hunsumoses7@gmail.com>
2024-02-20 09:58:10 +0100

    Revert "Used Refs to select DOM elements"
    This reverts commit 627e0b6dcf1fab6d6eb76c31241c9e37e5a9464a.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 import { useMovies } from "./useMovies";
 
@@ -113,30 +113,10 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
-  const inputEl = useRef(null);
-
-  useEffect(
-    function () {
-      function callback(e) {
-        if (document.activeElement === inputEl.current) return;
-
-        if (e.code === "Enter") {
-          inputEl.current.focus();
-          setQuery("");
-        }
-      }
-
-      document.addEventListener("keydown", callback);
-      return () => document.addEventListener("keydown", callback);
-    },
-    [setQuery]
-  );
-
-  //   useEffect(function () {
-  //     const el = document.querySelector(".search");
-  //     console.log(el)
-  //     el.focus();
-  //   }, []);
+  useEffect(function () {
+    const el = document.querySelector(".search");
+    el.focus();
+  }, []);
 
   return (
     <input
@@ -145,7 +125,6 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
-      ref={inputEl}
     />
   );
 }
